9b3c9d9b44362ddd876c933bacee9658482f845a
Lewis Kindeleit <lewiskindeleit@hotmail.com>
2015-01-15 11:04:08 +0000

    Basic code to implement all modules

---
+++ master_file.jl
@@ -0,0 +1,17 @@
+println("Enter X size of environment: ")
+x_size = int(readline(STDIN))
+println("Enter Y size of environment: ")
+y_size = int(readline(STDIN))
+println("Enter amount of starting cells: ")
+cells = int(readline(STDIN))
+println("Enter proposed speed of cells: ")
+speed = float(readline(STDIN))
+println("Enter amount of timesteps: ")
+steps = int(readline(STDIN))
+
+
+
+for i = 1:steps
+#Call everyones stuff
+end
+
