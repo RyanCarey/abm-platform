9ae269919d9293e2432a460bb3c5fdb932086fef
Lewis Kindeleit <lewiskindeleit@hotmail.com>
2015-01-15 14:31:45 +0000

    Module to display cells

--- display.jl
+++ display.jl
@@ -4,11 +4,11 @@ function display(locations)
 
 x = locations[:, 1]
 y = locations[:, 2]
-myplot = plot(x = x, y = y)
-return myplot
+plot(x, y, "ro")
 end
 
 locations = rand(10, 2)
 
-using Gadfly
-draw(display(locations))
+using Winston
+display(locations)
+
