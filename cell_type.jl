13c8b6d7618ae620f21d55e3afe4e06bf57a269e
RyanCarey <ry.duff@gmail.com>
2015-01-22 14:40:17 +0000

    consolidated types, remaking move.jl

--- cell_type.jl
+++ cell_type.jl
@@ -1,7 +1,12 @@
+type Point
+	x::Real
+	y::Real
+	Point(x, y) = new(x, y)
+end
+
 type Cell
 	loc::Point
 	r::Real
 	angle::Real
 	speed::Real
- 
 end
--- point_type.jl
+++
@@ -1,6 +0,0 @@
-type Point
-	x::Real
-	y::Real
-
-	Point(x, y) = new(x, y)
-end
