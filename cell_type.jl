12ac6f836577a3469dabfef42b84b21141d69e83
Lewis Kindeleit <lewiskindeleit@hotmail.com>
2015-01-22 13:49:02 +0000

    Removed name variable

--- cell_type.jl
+++ cell_type.jl
@@ -1,15 +1,7 @@
 type Cell
-	name::String
 	loc::Point
-	radius::Real
+	r::Real
 	angle::Real
 	speed::Real
  
-	#Cell(loc, radius, angle, speed) = new(loc, radius, angle, speed)    	
-	#Cell(name, loc, radius, angle, speed) = new(name, loc, radius, angle, speed)
-
-	function getAngle()
-		println("Angle: ", angle)
-	end
-	this.method = getAngle()
 end
