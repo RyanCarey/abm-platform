754a0beefcff3fb91e8845dff4aaf0eff36866c5
Lewis Kindeleit <lewiskindeleit@hotmail.com>
2015-01-26 14:39:04 +0000

    Some changes made, overlap still not working

--- move.jl
+++ move.jl
@@ -18,10 +18,11 @@ function propose_move_x(conc_map, cell::Cell, speed_param::Float64)
 end
 
 function move_any!(conc_map::Array, X::Array, max_speed::Float64)
-  n = length(X)
-  m = rand(1:n)
-  move_cell_x!(conc_map, X, m, max_speed)
-  return X
+	n = length(X)
+	m = rand(1:n)
+	
+	move_cell_x!(conc_map, X, m, max_speed)
+	return X
 end
 
 function move_cell_x!(conc_map, X::Array, m::Int, max_speed::Float64)
@@ -29,25 +30,22 @@ function move_cell_x!(conc_map, X::Array, m::Int, max_speed::Float64)
   cell = X[m]
   cell, final = propose_move_x(conc_map, cell, max_speed)
   cell = check_borders(cell, final)
-  X[m] = cell
-  print(X)
   if is_overlap(X, m)
-    X[m] = cell
-    X[m].angle = 0.
-    X[m].speed = 0.
-    #move_cell_x!(conc_map, X,m,max_speed) # include this to retry moving cell
-  end
+	#move_cell_x!(conc_map, X, m, 0.2) # include this to retry moving cell. Small speed so it only just moves a little.
+	
+  else
+	X[m] = cell
+	print(X)
+	end
 end
 
-function is_overlap(X::Array, m::Int)  # will later need to add multiple radii
-  n = length(X)
-  distances = zeros(n,3)
-  for i in 1:n
-    if (X[n].loc.x - X[m].loc.x)^2 + (X[n].loc.y - X[m].loc.y)^2 < (X[n].r + X[m].r)^2
-      if n != m
-        return true
-      end
-    end
-  end
-  return false
+function is_overlap(X::Array, m::Int)  
+	n = length(X)
+
+	for i in 1:n
+		if n != m && sqrt((X[n].loc.x - X[m].loc.x)^2 + (X[n].loc.y - X[m].loc.y)^2) < X[n].r + X[m].r
+			return true
+   		end
+ 	end
+	return false
 end
