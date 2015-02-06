1256bbf83347c064f7cd7f1be4eec947c6ae248a
RyanCarey <ry.duff@gmail.com>
2015-02-06 13:02:41 +0000

    making click controls

--- test2.jl
+++ test2.jl
@@ -1,7 +1,7 @@
 using Base.Graphics
 using Cairo
 using Tk
-include("abm-platform/pause.jl")
+include("pause.jl")
 
 win = Toplevel("Test", 400, 200)
 c = Canvas(win)
@@ -12,43 +12,57 @@ global is_blue = true
 
 # Set coordinates to go from 0 to 10 within a 300x100 centered region
 
-function hello_world()
+global message = ""
+function hello()
   is_blue = true
-  #println("hello world")
+  println(message)
+  
 end
 
-function blue_box(a,b,width,height)
+#=
+function box(a,b,width,height,is_blue)
   hello_world()
   ctx = getgc(c)
   set_coords(ctx, a, b, width, height, 0, 10, 0, 10)
   redness = !is_blue*.7
   blueness = is_blue*.8
-  set_source_rgb(ctx, 0, 0, 1)   # set color to blue
+  println("r: ",redness)
+  println("b: ",blueness)
+  set_source_rgb(ctx, redness, 0, blueness)   # set color to blue
   paint(ctx)                     # paint the entire clip region
+  return is_blue
 end
 
-#blue_box(50,50,50,50)
-#blue_box(150,150,50,50)
 
-function blue_boxes()
-  for i in linspace(0,180,10)
-    for j in linspace(0,180,10)
-      blue_box(i,j,10,10)
+function boxes(is_blue)
+  #is_blue = !is_blue
+  for i in linspace(0,180,3)
+    for j in linspace(0,180,3)
+      is_blue = box(i,j,10,10,is_blue)
+      is_blue = !is_blue
     end
+    println("blue as at end of row: ",is_blue)
   end
+  println("blue as at end of click: ",is_blue)
+  return !is_blue
+end
+=#
+
+
+
+function precall(c,x,y)
+  callback(c,x,y,hello)
 end
 
+global is_blue = true
+c.mouse.button1press = precall
 
-#blue_box(310,310,320,320)
 
-
-c.mouse.button1press = (c,x,y) -> callback(c,x,y)
-
-function callback(c,x,y)
+function callback(c,x,y,is_blue)
   println("click: ",x,",",y)
-  blue_boxes()
+  println("blue as at time of click: ",is_blue)
+  is_blue = boxes(is_blue)
 end
-
 
 # keeps program open
 if !isinteractive()
