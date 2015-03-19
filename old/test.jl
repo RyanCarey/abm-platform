0bc322a3009ea5646ec44c55c365e1ec19618021
RyanCarey <ry.duff@gmail.com>
2015-03-19 21:07:34 +0000

    cleaning up

--- old/test.jl
+++
@@ -1,3 +0,0 @@
-a = [2 3;4 5;6 7]
-b = [true;true;false]
-a[:,1][b]
--- old/test_is_overlap.jl
+++
@@ -1,9 +0,0 @@
-include("move.jl")
-include("angle.jl")
-
-
-
-a = 10*rand(10,2)
-b = rand(10,1)
-X = [a b]
-is_overlap(X, 1,1.0)
